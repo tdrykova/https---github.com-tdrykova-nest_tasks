c5202816b0013f5f3448eb10bdf5852ad0cc208c
tdrykova <tdrykova@edu.hse.ru>
2024-03-04 10:39:41 +0300

    init tasks

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { title } from 'process';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService: TasksService) { }
+    
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks()
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('desc') desc: string
+    ): Task {
+        return this.tasksService.createTask(title, desc)
+    }
+}
+ 
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = []
+
+    getAllTasks(): Task[] {
+        return this.tasks
+    }
+
+    createTask(title: string, desc: string): Task {
+        const task: Task = {
+            id: uuidv4(),
+            title: title,
+            desc: desc,
+            status: TaskStatus.OPEN
+        }
+
+        this.tasks.push(task)
+        return task
+    }
+
+}
